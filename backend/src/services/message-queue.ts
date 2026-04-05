5b73aa662ed57f98fc8ae449cbe7cf2ae8bbde3c
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2026-04-05 12:30:30 +0300

    RabbitMQ Service implemented

--- backend/src/services/message-queue.ts
+++ backend/src/services/message-queue.ts
@@ -1,25 +1,60 @@
-import amqp from 'amqplib';
+import { connect, type ChannelModel } from 'amqplib';
 
-const URL = process.env.RABBIT_URL;
+const RABBIT_USER = process.env.RABBIT_USER;
+const RABBIT_PASSWORD = process.env.RABBIT_PASSWORD;
+const RABBIT_HOST = process.env.RABBIT_HOST;
+const RABBIT_PORT = process.env.RABBIT_PORT;
 
-if (!URL) {
-    throw new Error('RabbitMQ url not initialized as environment variable');
+if (!RABBIT_USER || !RABBIT_PASSWORD || !RABBIT_HOST || !RABBIT_PORT) {
+    throw new Error(
+        'RabbitMQ Parameters not initialized as environment variables'
+    );
 }
 
-let connection;
-let channel;
+const connection = await startConnection();
+const channel = await startChannel(connection);
 
-async function initRabbit() {
-    if (connection && channel) return;
+async function startConnection() {
+    console.log('About to start the RabbitMQ connection');
+    try {
+        return await connect(
+            `amqp://${RABBIT_USER}:${RABBIT_PASSWORD}@${RABBIT_HOST}:${RABBIT_PORT}/`
+        );
+    } catch (e) {
+        console.error('Error with connecting to rabbitmq: ', e);
+        throw new Error('RabbitMQ Connection Failed');
+    } finally {
+        console.log('RabbitMQ: Connection Created Succesfully');
+    }
+}
 
-    connection = await amqp.connect(URL);
+async function startChannel(connection: ChannelModel) {
+    console.log('About to create channel...');
+    try {
+        return await connection.createChannel();
+    } catch (e) {
+        console.error('Error with starting the channel:', e);
+        throw new Error('RabbitMQ Channel Creation Failed');
+    } finally {
+        console.log('RabbitMQ: Channel created succesfully');
+    }
+}
 
-    channel = await connection.createChannel();
+export async function addMessageToQueue<T extends object>(message: T) {
+    try {
+        const queueName = 'MESSAGES';
 
-    process.once('SIGINT', async () => {
-        console.log('\nClosing RabbitMQ connection...');
-        await channel.close();
-        await connection.close();
-        process.exit(0);
-    });
+        await channel.assertQueue(queueName, {
+            durable: true,
+        });
+
+        channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+            persistent: true,
+        });
+
+        console.log(`Sent to ${queueName}:`, message);
+    } catch (e) {
+        console.error('Failed to add message to queue:', e);
+        throw new Error('Failed');
+    }
 }
